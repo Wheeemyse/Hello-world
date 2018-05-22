e411facc2e6879dc66c4f776d9a5b4f39ee4b277
wheeemyse <wheemyse>
2018-05-22 17:30:56 +0200

    fghgf

--- Formulaire/formulaire.php
+++ Formulaire/formulaire.php
@@ -1,16 +1,4 @@
 <?php
-  $recipient = "talla.m.seck@gmail.com";
-  $fmtMerci= implode("", file("merci.htt"));
-  $fmtMail= implode("", file("mail.htt"));
-
-  foreach($_POST as $key=> $val) {
-  $fmtMerci= str_replace("<$key>", $val, $fmtMerci);
-  $fmtMail= str_replace("<$key>", $val, $fmtMail);
-  }
-
-  if ($_POST["access"] == "stopspam") {
-  mail($recipient, $_POST["subject"], $fmtMail);
-  }
-
-  echo $fmtMerci;
-?>
+  $to= 'talla.m.seck@gmail.com'
+  $subject= 'Formulaire simplon'
+ ?>
